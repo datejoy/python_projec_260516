283c132b1fc12bdb1491d534fa8f43f77b7bbe11
DateJoy <mokuhyouhahon@gmail.com>
2026-06-06 16:22:52 +0800

    DataFrame中的function

--- 260606/lesson3.py
+++ 260606/lesson3.py
@@ -120,14 +120,20 @@ def main():
 
     # 使用 Response 中的 preperty 為 ststus_code，如果取得的數字是200則成功，反之則失敗
     if response.status_code == 200:
-        
+
         # 使用 Response 實體中的json()實體方法，會傳出list的資料結構
         data:list[dict] = response.json()
 
         # list[dict] -> DataFrame
-        df = pd.DataFrame(data)
+        # 引數名稱呼叫 ex: pd.DataFrame(data = data)
+        # 引數值呼叫: 一般直接放引數
+        # df為DataFrame的實體
+        # 實體中有: 實體屬性、實體方法
+        df:pd.DataFrame = pd.DataFrame(data)
+        # Iterable 可重複讀取 (list, dict, dataframe)
 
-        print(df.head())
+        print(df.head(10))
+        print(df.tail(10))
 
         output_file = Path(__file__).with_name("youbike_report.pdf")
         export_to_pdf(df, output_file)
